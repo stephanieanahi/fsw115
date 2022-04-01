4218947707766a205894c7b6715c91130623b147
stephanieanahi <stephanie.guzman@bryanuniversity.edu>
2022-03-31 22:07:29 -0500

    modified files

--- week2/api.js
+++ week2/api.js
@@ -1,4 +1,11 @@
 
-fetch('https://opentdb.com/api.php?amount=10')
-
-
+const api ='https://opentdb.com/api.php?amount=10'
+async function getISS() {
+    const response = await fetch (api);
+    const data = await response.json();
+    console.log(data);
+    const {results} = data
+    document.getElementById('res').textContent=results;
+}
+ getISS();
+ 
