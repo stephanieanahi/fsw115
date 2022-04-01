b73c87656756b2c8391ba50d7a3cfa58bfa6957c
stephanieanahi <stephanie.guzman@bryanuniversity.edu>
2022-04-01 05:17:03 -0500

    modified files

--- week2/api.js
+++ week2/api.js
@@ -1,11 +1,23 @@
 
-const api ='https://opentdb.com/api.php?amount=10'
-async function getISS() {
-    const response = await fetch (api);
-    const data = await response.json();
-    console.log(data);
-    const {results} = data
-    document.getElementById('res').textContent=results;
-}
- getISS();
+fetch('https://fakestoreapi.com/products').then((data)=>
+{
+    return data.json();
+}).then((completedata)=>{
+    //console.log(completedata[2].title);
+    let data1="";
+    completedata.map((values)=>{
+        data1+=  `<div class="card">
+        <h1 class="title">${values.title}</h1>
+        <img src= ${values.image} alt="img" class="images">
+        <p>${values.description}</p>
+        <p class="category">${values.category}</p>
+        <p class="price">${values.price}</p>
+    </div>`
+    });
+    document.getElementById("cards").innerHTML=data1;
+   
+}).catch((err)=>{
+    console.log(err);
+})
+
  
